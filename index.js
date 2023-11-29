3dab2a1921faad12753d961873cc846a9ee14d1d
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2023-11-29 14:34:16 +0000

    feat: impleted update dom to see images and winner

--- index.js
+++ index.js
@@ -1,46 +1,51 @@
 const getRandomMove = () => {
-    const moves = ["rock", "paper", "scissors"];
-    const randomIndex = Math.floor(Math.random() * moves.length);
-    return moves[randomIndex];
-  };
-  
-  const getOutcome = (moveOne, moveTwo) => {
-    if (moveOne === moveTwo) {
-      return "It's a draw!";
-    }
-  
-    if (
-      (moveOne === "scissors" && moveTwo === "paper") ||
-      (moveOne === "rock" && moveTwo === "scissors") ||
-      (moveOne === "paper" && moveTwo === "rock")
-    ) {
-      return "Player One wins!";
-    }
-  
-    return "Player Two wins!";
-  };
-  
-  // Removing elements (nodes) from the DOM
-  const resetGame = () => {
-    if (document.getElementById("outcome")) {
-      const outcome = document.body.lastChild;
-      document.body.removeChild(outcome);
-    }
-  };
-  
-  const playGame = () => {
-    resetGame();
-    const playerOneMove = getRandomMove();
-    const playerTwoMove = getRandomMove();
-    const outcome = getOutcome(playerOneMove, playerTwoMove);
-    updateDOM(playerOneMove, playerTwoMove, outcome);
-  };
-  
-  const updateDOM = (moveOne, moveTwo, outcome) => {
-    // TODO Implement this method to update the DOM
-    // There are some images you can use in the images directory
-  };
-  
-  const playButton = document.getElementById("play-btn");
-  playButton.addEventListener("click", playGame);
-  
+  const moves = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * moves.length);
+  return moves[randomIndex];
+};
+
+const getOutcome = (moveOne, moveTwo) => {
+  if (moveOne === moveTwo) {
+    return "It's a draw!";
+  }
+
+  if (
+    (moveOne === "scissors" && moveTwo === "paper") ||
+    (moveOne === "rock" && moveTwo === "scissors") ||
+    (moveOne === "paper" && moveTwo === "rock")
+  ) {
+    return "Player One wins!";
+  }
+
+  return "Player Two wins!";
+};
+
+// Removing elements (nodes) from the DOM
+const resetGame = () => {
+  if (document.getElementById("outcome")) {
+    const outcome = document.body.lastChild;
+    document.body.removeChild(outcome);
+  }
+};
+
+const playGame = () => {
+  resetGame();
+  const playerOneMove = getRandomMove();
+  const playerTwoMove = getRandomMove();
+  const outcome = getOutcome(playerOneMove, playerTwoMove);
+  updateDOM(playerOneMove, playerTwoMove, outcome);
+};
+
+const updateDOM = (moveOne, moveTwo, outcome) => {
+  const playerOneImgElem = document.getElementById("player-one-move__img");
+  playerOneImgElem.src = `/images/${moveOne}.png`;
+
+  const playerTwoImgElem = document.getElementById("player-two-move__img");
+  playerTwoImgElem.src = `/images/${moveTwo}.png`;
+
+  const winner = document.getElementById("winner");
+  winner.innerText = outcome;
+};
+
+const playButton = document.getElementById("play-btn");
+playButton.addEventListener("click", playGame);
